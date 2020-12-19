0c7b0f0e2e067e6b81c54ba43e2bcf9d45720807
RAHUL KUMAR SINGH <singhrash23@gmail.com>
2020-12-19 15:43:44 +0530

    .

---
+++ assets/js/project.js
@@ -0,0 +1,55 @@
+/* Project Cards */
+
+const projectcards = document.querySelector(".projectcards");
+const projects = [
+  {
+    title: "Smart Cooking System",
+    cardImage: "assets/images/project-page/smartcooking.png",
+    description:
+      "Its a Machine learning based project which is used to cook food on your finger press!",
+    Previewlink: "#",
+    Githublink: "#",
+  },
+  {
+    title: "Home Security System",
+    cardImage: "assets/images/project-page/homesecurity.png",
+    description:
+      "An IOT project to keep your home secure in my absence and in presence as well.",
+    Previewlink: "https://github.com/users/singhrash23/projects/1",
+    Githublink: "https://github.com/users/singhrash23/projects/1",
+  },
+  {
+    title: "Face lock",
+    cardImage: "assets/images/project-page/face.png",
+    description:
+      "A facial lock system for the user in this first user creates an data set of his face and then using that we create an lock system that is able to identify the user!",
+    Previewlink: "#",
+    Githublink: "#",
+  },
+];
+
+const showCards = () => {
+  let output = "";
+  projects.forEach(
+    ({ title, cardImage, description, Previewlink, Githublink }) =>
+      (output += `
+    <div class="skill-card card">
+      <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
+        <div class="header">
+        </div>
+        <div class="data">
+          <div class="content">
+            <h1 class="title"><a href="#">${title}</a></h1>
+            <p class="text">${description}</p>
+            <ul class="menu-content">
+              <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
+              <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>`)
+  );
+  projectcards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
